cba9821901156f2df96b6b6e0031760474cf27df
sbigelo <sean.bigelow1@gmail.com>
2021-08-25 15:51:05 -0400

    added final migrations. addded application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,28 @@
 class ApplicationController < ActionController::API
+    before_action :authorized
+
+
+
+    def encode_token
+        JWT.encode(payload, ENV['JWT_SECRET'])
+    end
+
+    def auth_headder
+        request.headers['Authorization']
+    end
+
+    def current_user
+        if decoded_token
+            user_id = decoded_token[0]['user_id']
+            @user = User.find_by(id: user_id)
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
+    def authorized
+        render json: {message: "Please log in"}, status: :unauthorized unless logged_in?
+    end
+
 end
--- db/migrate/20210825184024_create_bills.rb
+++ db/migrate/20210825184024_create_bills.rb
@@ -1,7 +1,11 @@
 class CreateBills < ActiveRecord::Migration[6.1]
   def change
     create_table :bills do |t|
-
+      t.string :company_name
+      t.string :due_date
+      t.string :amount_due
+      t.string :notes
+      t.boolean :paid_status
       t.timestamps
     end
   end
