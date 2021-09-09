d1c4f74131f655e299b2f1523768e962ae314dab
sbigelo <sean.bigelow1@gmail.com>
2021-09-09 16:33:42 -0500

    added proper routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :users
   resources :bills
   post '/login', to: 'sessions#create'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
 end
