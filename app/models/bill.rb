3fc8cec50b6b7b9057c1fbce449757871413df26
sbigelo <sean.bigelow1@gmail.com>
2021-09-09 12:51:23 -0500

    added active record associations and validations

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,2 +1,3 @@
 class Bill < ApplicationRecord
+    belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    has_secure_password
+    has_many :bills
+    validates :first_name, :last_name, :email, presence: true
+    validates :email, uniqueness: {case_sensitive: false}
 end
